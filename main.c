6addf6d6354d41624368b439bea1914c8f95e8df
SoohyeonHong <55083734+SoohyeonHong@users.noreply.github.com>
2019-10-10 15:29:13 +0900

    01

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void print_star()
+{ 	
+	//함수 정의 
+	
+	int i; 
+		
+	for( i = 0; i < 10; i++ ) 
+		printf("*"); 
+	
+}
+
+
+int main(int argc, char *argv[]) {
+	
+	
+	int i; 
+	
+	// 함수 호출  
+	
+	print_star();
+	print_star();
+	print_star();
+	
+	
+	return 0;
+}
