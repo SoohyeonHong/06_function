d2352c0c7e854b623c8dacf0d819fb13fbf9053e
SoohyeonHong <55083734+SoohyeonHong@users.noreply.github.com>
2019-10-10 15:52:01 +0900

    02

--- main.c
+++ main.c
@@ -3,26 +3,32 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void print_star()
-{ 	
-	//define function 
+int sumTwo( int a, int b)
+{
+	/*
+	int output;
+	output = a + b;
+	return output;
+	*/
 	
-	int i; 
-		
-	for( i = 0; i < 10; i++ ) 
-		printf("*"); 
-	
+	return a+b;
 }
 
+int square(int n)
+{
+	return n*n;
+}
 
+int get_max( int a, int b )
+{
+	if ( a>b ) 
+		return a; // 어차피 a나오면 여기서 끝나게 됨 
+	//else 
+		return b;
+	
+}
 int main(int argc, char *argv[]) {
-	
-	// using function  
-	
-	print_star();
-	print_star();
-	print_star();
-	
+	 
 	
 	return 0;
 }
